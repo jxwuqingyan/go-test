7962310fd44a2782009ccc7cac8ba4752ba7ff7d
wuqingyan <1131169640@qq.com>
2024-07-21 21:16:55 +0800

    Update xcompile.sh

--- scripts/xcompile.sh
+++ scripts/xcompile.sh
@@ -3,7 +3,7 @@
 set -e
 # you may need to go install github.com/mitchellh/gox@v1.0.1 first
 echo $VERSION
-CGO_ENABLED=0 gox -ldflags "-s -w ${LDFLAGS}" -output="build/easycmd_{{.OS}}_{{.Arch}}" --osarch="darwin/amd64 darwin/arm64 linux/386 linux/amd64 linux/arm linux/arm64"
+CGO_ENABLED=0 gox -ldflags "-s -w ${LDFLAGS}" -output="build/go-test_{{.OS}}_{{.Arch}}" --osarch="darwin/amd64 darwin/arm64 linux/386 linux/amd64 linux/arm linux/arm64"
 
 cd build
 
